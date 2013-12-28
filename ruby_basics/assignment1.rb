501fdcac1a5a88bb8fc965e3169b284e8fa9ea20
Sharang Dashputre <sharang.dashputre@weboniselab.com>
2013-12-28 14:13:26 +0530

    questions 5 to 20 complete

--- ruby_basics/assignment1.rb
+++ ruby_basics/assignment1.rb
@@ -1,3 +1,20 @@
+require 'date'
+
+# modified from activesupport
+def ordinalize(number)
+  suffix = if (11..13).include?(number % 100)
+             'th'
+           else
+             case number % 10
+             when 1; 'st'
+             when 2; 'nd'
+             when 3; 'rd'
+             else    'th'
+             end
+           end
+  "#{number}#{suffix}"
+end
+
 def make_parts(string1, number_of_parts)
   part_size, extra_chars = string1.size.divmod number_of_parts
   parts = (1..number_of_parts).to_a.map do |part_number| 
@@ -39,7 +56,9 @@ puts string1 + string2
 puts '10) Print current date.'
 puts Time.now.strftime('%d %B %Y')
 
-puts '11) print 12th Jan 2012'
+puts '11) print 12th Jan 2012' # !?
+date = Date.parse('12th Jan 2012')
+puts date.strftime("#{ordinalize(date.day)} %b %Y")
 
 puts '12) add 7 days in current date'
 puts (Time.now + (60 * 60 * 24 * 7)).strftime('%d %B %Y')
@@ -54,6 +73,8 @@ puts '15) Remove the HTML characters from string.'
 puts "String1 with HTML tags removed: #{string1.gsub('<br/>','')}"
 
 puts "16) Print the 'RUBY' word from string 1 by traversing it using string functions"
+# considering "RUBY" to be an independent word and not a part of a word
+string1.split.each { |word| puts word if word=='RUBY' }
 
 puts '17) Find the length of string 1 & 2.'
 puts "Length of string1: #{string1.length}\nLength of string2: #{string2.length}"
@@ -64,10 +85,17 @@ puts '/^[a-z][a-z0-9_\-.]+@weboniselab\.com$/i'
 puts "22) write regex for phone no format : '+78' followed by 8 nos. e.g. +7898328732"
 puts '/^\+78\d{8}$/'
 
+puts '18) Compare two dates. (12-04-2010 & 12-05-2011). Calculate the days between these two dates.'
+puts "Number of days in between: #{(Date.parse('12-05-2011') - Date.parse('12-04-2010')).to_i}"
+
+puts '19) Print date after 20 days from current date'
+puts (Time.now + (60 * 60 * 24 * 20)).strftime('%d %B %Y')
+
+puts '20) Print date in array format.'
+today = Date.today
+p [today.day, today.month, today.year]
+
 =begin
-18) Compare two dates. (12-04-2010 & 12-05-2011). Calculate the days between these two dates.
-19) Print date after 20 days from current date
-20) Print date in array format.
 23) finding site name and type from a url string using regex
  http://www.xyz.com/classid/17950142?type=mandatory
 24) write regex for this format
