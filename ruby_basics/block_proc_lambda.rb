76c8e147e6140c2df3be60cfe76a528b038b72e1
Sharang Dashputre <sharang.dashputre@weboniselab.com>
2013-12-30 16:26:13 +0530

    Made column width <= 79

--- ruby_basics/block_proc_lambda.rb
+++ ruby_basics/block_proc_lambda.rb
@@ -1,6 +1,7 @@
-puts "- Blocks are not objects
-- Procs and Lambdas are both objects of class Proc"
-puts "- A block is the collection of statements that come between '{' and '}' or 'do' and 'end'"
+print "- Blocks are not objects
+- Procs and Lambdas are both objects of class Proc
+- A block is the collection of statements that come between '{' and '}'"
+puts " or 'do' and 'end'"
 puts "- Blocks cannot be assigned to variables unlike Procs and Lambdas"
 puts '- Only one block can be passed as a parameter unlike Procs and Lambdas'
 
@@ -27,7 +28,11 @@ end
 
 puts "Value returned when proc_inside is called: #{proc_inside}"
 puts "Value returned when lambda_inside is called: #{lambda_inside}"
-puts '- This shows that Procs return from the method from where they are invoked when a "return"'
-puts 'is encountered whereas lamdbas just continue execution from where they were invoked'
-puts "- Another difference between Procs and Lambdas is that Lambdas throw an Exception when the"
-puts "number of arguments passed don't match the number of arguments specified in the lambda body"
+print '- This shows that Procs return from the method from where they are invok'
+puts 'ed when a "return"'
+print 'is encountered whereas lamdbas just continue execution from where they w'
+puts 'ere invoked'
+print '- Another difference between Procs and Lambdas is that Lambdas throw an E'
+puts 'xception when the'
+print "number of arguments passed don't match the number of arguments specified"
+puts ' in the lambda body'
