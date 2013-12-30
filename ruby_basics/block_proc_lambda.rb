10a6e3202d368360a41ee1d03e95bc1b8f842a8a
Sharang Dashputre <sharang.dashputre@weboniselab.com>
2013-12-30 14:43:35 +0530

    Block vs Proc vs Lambda

---
+++ ruby_basics/block_proc_lambda.rb
@@ -0,0 +1,33 @@
+puts "- Blocks are not objects
+- Procs and Lambdas are both objects of class Proc"
+puts "- A block is the collection of statements that come between '{' and '}' or 'do' and 'end'"
+puts "- Blocks cannot be assigned to variables unlike Procs and Lambdas"
+puts '- Only one block can be passed as a parameter unlike Procs and Lambdas'
+
+puts "---------------------------------\nConsider the following methods:
+def proc_inside
+  proc { return 'return from proc' }.call
+  return 'return from method proc_inside'
+end
+
+def lambda_inside
+  ->{ return 'return from lambda' }.call
+  return 'return from method lambda_inside'
+end\n----------------------------------"
+
+def proc_inside
+  proc { return 'return from proc' }.call
+  return 'return from method proc_inside'
+end
+
+def lambda_inside
+  ->{ return 'return from lambda' }.call
+  return 'return from method lambda_inside'
+end
+
+puts "Value returned when proc_inside is called: #{proc_inside}"
+puts "Value returned when lambda_inside is called: #{lambda_inside}"
+puts '- This shows that Procs return from the method from where they are invoked when a "return"'
+puts 'is encountered whereas lamdbas just continue execution from where they were invoked'
+puts "- Another difference between Procs and Lambdas is that Lambdas throw an Exception when the"
+puts "number of arguments passed don't match the number of arguments specified in the lambda body"
